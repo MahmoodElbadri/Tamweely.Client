b8389fdad44fd6b6c0cbee5d1a642daa14f2d951
Mahmood Elbadri <mahmoud.elbadry357@gmail.com>
2025-12-27 16:25:41 +0200

    added sweet alert

--- src/app/features/address-book/address-list/address-list.component.ts
+++ src/app/features/address-book/address-list/address-list.component.ts
@@ -5,6 +5,8 @@ import { AddressBookDto } from '../models/address-book-dto';
 import { DatePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { environment } from '../../../../environments/environment.development';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-address-list',
@@ -17,6 +19,7 @@ export class AddressListComponent implements OnInit {
   //injections
   addressService = inject(AddressBookService);
   router = inject(Router);
+  toastr = inject(ToastrService);
 
   //variables
   addresses: AddressBookDto[] = [];
@@ -24,6 +27,44 @@ export class AddressListComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllAddressBooks();
+  }
+
+  onDelete(id: number) {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-danger mx-2', // Style valid for your Bootstrap/Custom CSS
+        cancelButton: 'btn btn-secondary mx-2'
+      },
+      buttonsStyling: false
+    });
+
+    swalWithBootstrapButtons.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      reverseButtons: true // Puts "Cancel" on the left, "Delete" on the right
+    }).then((result) => {
+      
+      if (result.isConfirmed) {
+        // User clicked "Yes", so we call the API
+        this.addressService.deleteAddressBook(id).subscribe({
+          next: () => {
+            // 1. Show success message (using Toastr to keep it quick)
+            this.toastr.success('Address deleted successfully');
+            
+            // 2. Refresh the list (remove item from array locally to avoid API call)
+            this.addresses = this.addresses.filter(a => a.id !== id);
+          },
+          error: (err) => {
+            this.toastr.error('Failed to delete address');
+            console.error(err);
+          }
+        });
+      }
+    });
   }
 
   getAllAddressBooks() {
